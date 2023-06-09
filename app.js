0b5aa92998e85cc0f969585aa4895ce7d9b3d8f6
lauqui08 <quismorio27@gmail.com>
2023-06-09 23:16:33 +0800

    Beta Tictactoe is done

---
+++ app.js
@@ -0,0 +1,39 @@
+const items = document.querySelectorAll('td');
+let player = 'O';
+let isWinner = false;
+let moves = 0;
+items.forEach(item => {
+    item.addEventListener('click',(event) => {
+        moves++;
+        console.log(moves);
+        if(moves === 9){
+            event.target.textContent = player;
+            alert('Draw!');
+        }else if(!isWinner){
+            if(event.target.textContent === '' && player === 'X'){
+                event.target.textContent = player;
+                checkWinner(player);
+                player = 'O';
+            }else if(event.target.textContent === '' && player === 'O'){
+                event.target.textContent = player;
+                checkWinner(player);
+                player = 'X';
+            }
+        }
+       
+    });
+});
+
+function checkWinner(player){
+   if(items[0].textContent === player &&  items[1].textContent === player && items[2].textContent === player ||
+    items[3].textContent === player &&  items[4].textContent === player && items[5].textContent === player ||
+    items[6].textContent === player &&  items[7].textContent === player && items[8].textContent === player ||
+    items[0].textContent === player &&  items[4].textContent === player && items[8].textContent === player ||
+    items[2].textContent === player &&  items[4].textContent === player && items[6].textContent === player ||
+    items[0].textContent === player &&  items[3].textContent === player && items[6].textContent === player ||
+    items[1].textContent === player &&  items[4].textContent === player && items[7].textContent === player ||
+    items[2].textContent === player &&  items[5].textContent === player && items[8].textContent === player){
+    isWinner = true;
+            alert(`${player} Wins`);
+   }
+};
